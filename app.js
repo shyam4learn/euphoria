515e8a8ead00ca612e0a9fa1f8d2eaf604522ef2
shyam4learn <shyam4learn@gmail.com>
2017-05-16 22:50:40 -0400

    removed /schedule

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.use(express.static('src/views'));
 
 
 app.get('/', function(req, res){
-  res.send('Hello!')
+  // res.send('Hello!');
 });
 
-app.get('/schedule', function(req, res){
-  res.send('Books!')
-});
+// app.get('/schedule', function(req, res){
+//   res.send('Books!');
+// });
 
 app.listen(port || 5000, function(err){
   console.log('Running server on:' + port);
